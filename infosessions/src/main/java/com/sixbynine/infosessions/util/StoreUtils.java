5b9bde50475ff96db7eedfee82d39bfadfd5e8b3
steviek <stevekideckel@gmail.com>
2015-01-13 23:44:23 -0500

    add share, rate to menu

--- infosessions/src/main/java/com/sixbynine/infosessions/util/StoreUtils.java
+++ infosessions/src/main/java/com/sixbynine/infosessions/util/StoreUtils.java
@@ -1,9 +1,11 @@
 package com.sixbynine.infosessions.util;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 
+import com.sixbynine.infosessions.R;
 import com.sixbynine.infosessions.app.MyApplication;
 
 /**
@@ -11,9 +13,26 @@ import com.sixbynine.infosessions.app.MyApplication;
  */
 public class StoreUtils {
 
-    public static Intent getStoreIntent(){
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse("market://details?id=" + MyApplication.getInstance().getPackageName()));
-        return intent;
+    public static String getShareMessage(){
+        final String appPackageName = MyApplication.getInstance().getPackageName();
+        return MyApplication.getInstance().getString(R.string.share_app_message,
+                "https://play.google.com/store/apps/details?id=" + appPackageName);
+    }
+
+    public static void shareApp(Activity context){
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_TEXT, getShareMessage());
+        intent.setType("text/plain");
+        context.startActivity(intent);
+    }
+
+    public static void launchStoreIntent(Activity context){
+        final String appPackageName = context.getPackageName();
+        try {
+            context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+        } catch (android.content.ActivityNotFoundException anfe) {
+            context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+        }
     }
 }
