67f8b78d16028ef22a4f7380cf0ed48084158958
steviek <stevekideckel@gmail.com>
2015-01-17 10:34:44 -0500

    prepare for amazon

--- infosessions/src/main/java/com/sixbynine/infosessions/util/StoreUtils.java
+++ infosessions/src/main/java/com/sixbynine/infosessions/util/StoreUtils.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 
+import com.sixbynine.infosessions.BuildConfig;
 import com.sixbynine.infosessions.R;
 import com.sixbynine.infosessions.app.MyApplication;
 
@@ -15,8 +16,13 @@ public class StoreUtils {
 
     public static String getShareMessage(){
         final String appPackageName = MyApplication.getInstance().getPackageName();
-        return MyApplication.getInstance().getString(R.string.share_app_message,
-                "https://play.google.com/store/apps/details?id=" + appPackageName);
+        if(BuildConfig.AMAZON){
+            return MyApplication.getInstance().getString(R.string.share_app_message,
+                    "http://www.amazon.com/gp/mas/dl/android?id=" + appPackageName);
+        }else{
+            return MyApplication.getInstance().getString(R.string.share_app_message,
+                    "https://play.google.com/store/apps/details?id=" + appPackageName);
+        }
     }
 
     public static void shareApp(Activity context){
@@ -29,10 +35,19 @@ public class StoreUtils {
 
     public static void launchStoreIntent(Activity context){
         final String appPackageName = context.getPackageName();
-        try {
-            context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
-        } catch (android.content.ActivityNotFoundException anfe) {
-            context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+        if(BuildConfig.AMAZON){
+            try {
+                context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("amzn://apps/android?p=" + appPackageName)));
+            } catch (android.content.ActivityNotFoundException anfe) {
+                context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.amazon.com/gp/mas/dl/android?id=" + appPackageName)));
+            }
+        }else{
+            try {
+                context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+            } catch (android.content.ActivityNotFoundException anfe) {
+                context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+            }
         }
+
     }
 }
